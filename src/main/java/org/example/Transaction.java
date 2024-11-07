1caac18efeca2b55fc25de48b61fb114c25247e5
Samiaennaji <139915252+Samiaennaji@users.noreply.github.com>
2024-11-07 13:01:17 +0100

    first commit

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -1,6 +1,7 @@
 package org.example;
 import lombok.Getter;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -11,10 +12,13 @@ public class Transaction {
     private final String reference;
     private Set<Compte> comptes;
 
-    public Transaction(TransactionType T,Date timestu){
+    public Transaction(TransactionType T,Compte compte1,Compte compte2){
         this.T=T;
         this.reference=UUID.randomUUID().toString();
-        this.timestump=timestu;
+        this.timestump=new Date();
+        this.comptes=new HashSet<>();
+        this.comptes.add(compte1);
+        this.comptes.add(compte2);
 
     }
 }
