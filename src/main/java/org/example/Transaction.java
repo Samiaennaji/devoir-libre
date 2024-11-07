b95ac7be8682948e761f42303fb5f453b50e3edc
Samiaennaji <139915252+Samiaennaji@users.noreply.github.com>
2024-11-07 12:15:12 +0100

    first commit

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -2,10 +2,14 @@ package org.example;
 import lombok.Setter;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class Transaction {
     private final TransactionType T;
-
+    private final Date timestump;
+    private final String reference;
 }
