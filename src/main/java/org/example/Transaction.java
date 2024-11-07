b13febdb29be3834130c4969b676280ef2f2136b
Samiaennaji <139915252+Samiaennaji@users.noreply.github.com>
2024-11-07 17:02:54 +0100

    first commit

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -1,24 +1,27 @@
 package org.example;
 import lombok.Getter;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+
+import java.util.*;
 
 @Getter
 public class Transaction {
     private final TransactionType T;
     private final Date timestump;
     private final String reference;
-    private Set<Compte> comptes;
+    private List<Compte> comptes;
 
    //constructeur de transaction
-    public Transaction(TransactionType T,Compte compte1,Compte compte2){
+    public Transaction(Compte compte1,Compte compte2){
       //  this.T=T;
         this.reference=UUID.randomUUID().toString();
         this.timestump=new Date();
-        this.comptes=new HashSet<>();
+        this.comptes=new ArrayList<>();
         this.comptes.add(compte1);
         this.comptes.add(compte2);
+
+        if(compte1.getBanques().equals(compte2.getBanques())) T=TransactionType.VIREST;
+        else if (compte1.getBanques().getId().equals(compte2.getBanques().getId())) T=TransactionType.VIRIN;
+        else if (compte1.getBanques().getPays().equals(compte2.getBanques().getPays())) T=TransactionType.VIRMULTA;
+        else T=TransactionType.VIRCHA;
     }
 }
