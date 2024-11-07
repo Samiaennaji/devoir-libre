c132ab53748c8cc252a063c75e9883637ef0d79b
Samiaennaji <139915252+Samiaennaji@users.noreply.github.com>
2024-11-07 11:48:23 +0100

    first commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,15 @@
+package org.example;
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+
+public class Client {
+    private String numclient ;
+    private String nom;
+    private String prenom;
+    private String adress;
+    private int phone;
+    private String email;
+}
